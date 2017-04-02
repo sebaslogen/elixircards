249b5e14cd400d0a63f3fbd3c122911fd29617c6
Sebas LG <neoranga55@gmail.com>
2017-04-03 00:19:08 +0200

    Add functions: create, shuffle contains and deal

--- lib/elixircards.ex
+++ lib/elixircards.ex
@@ -12,7 +12,21 @@ defmodule Elixircards do
       :world
 
   """
-  def hello do
-    :world
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+    Enum.flat_map(suits, fn(suit) -> Enum.map(values, &("#{&1} of #{suit}")) end)
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member? deck, card
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
